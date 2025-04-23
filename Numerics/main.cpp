152e35829db9ddbb78731f2452824da1614f4435
Pelivan <Pelivan@DESKTOP-29KL5UV>
2025-04-23 21:22:30 +0300

    Numerics encoding

--- Numerics/main.cpp
+++ Numerics/main.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 void main()
 {
-	setlocale(LC_ALL, " ");
+	setlocale(LC_ALL, "");
 
+	int n[5], d;
+	cout << "Введите десятичное число: "; cin >> d;
+    
+	for (int i = 0; d > 0; i++)
+	
+	{
+		n[i] = d % 2;
+		d = d / 2;
+    
+		cout << n[i];
+	}
+      
+   cout << endl;
+ 
 
 }
+
