5b5d23492906f7f636032506f3c95e2e87028784
Pelivan <Pelivan@DESKTOP-29KL5UV>
2025-04-27 22:46:35 +0300

    2Darrays encoding

--- 2Darrays/main.cpp
+++ 2Darrays/main.cpp
@@ -3,24 +3,93 @@ using namespace std;
 
 void main()
 {
-	setlocale(LC_ALL, " ");
+	setlocale(LC_ALL, "");
 	cout << "2Darrays" << endl;
 	const int ROWS = 5;
 	const int COLS = 4;
-	int arr[ROWS][COLS] =
-	{
-		{1, 2, 3 },
-		{4, 5, 6 },
-		{7, 8, 9 },
-	};
+	int arr[ROWS][COLS]; 
+
 		for (int i = 0; i < ROWS; i++)
-		 
 		{
 			for (int j = 0; j < COLS; j++)
+			
 			{
-				cout << arr[i][j] << "\t";
-		    }
+				arr[i][j] = rand() % 100;
+				cout << arr[i][j]<< "\t";
+				
+				
+			}	
 			cout << endl;
 		}
-		B
+				
+		int s = 0;
+		
+		for (int i = 0; i < ROWS; i++)
+
+		{
+			for (int j = 0; j < COLS; j++)
+
+			{
+				s += arr[i][j];
+			}
+		}
+             cout << "Сумма элементов массива: " << s << endl;
+             cout << "Среднее-арифметическое элементов массива: " << (double)s / (COLS * ROWS) << endl;
+            
+
+
+			 
+			int min, max;
+		    min = max = arr[0][0];
+			 for (int i = 0; i < ROWS; i++)
+			 
+			 
+			 
+			 {
+			     for (int j = 0; j < COLS; j++)
+			     
+			     
+			     
+				 {
+					 if (arr[i][j] < min) min = arr[i][j];
+					 if (arr[i][j] > max) max = arr[i][j];
+
+				 }
+		     }
+             cout << "Минимальное значение элемента в массиве: " << min << endl;
+             cout << "Максимальное значение элемента в массиве: " << max << endl;
+			 cout << endl;
+
+
+			 int number_of_shifts_L;
+			 cout << "Введите количество сдвигов: "; cin >> number_of_shifts_L;
+			 for (int i = 0; i < number_of_shifts_L; i++)
+			 {
+				 for (int j = 0; j < number_of_shifts_L; j++)
+				 {
+					 int buffer = arr[i][0];
+					 for (int i = 0; i < ROWS; i++)
+					 {
+						 for (int j = 1; j < COLS; j++)
+						 
+						 {
+							 arr[i][j - 1] = arr[i][j];
+                         }
+							 arr[i][COLS - 1] = buffer;
+					     
+						 for (int i = 0; i < ROWS; i++)
+						 {
+							 for (int j = 0; j < COLS; j++)
+							 {
+                             cout << arr[i][j] << "\t";
+						     }
+							 cout << endl;
+						 }
+					 }
+
+			     }
+		     }
+
+
+
 }
--- Numerics/main.cpp
+++ Numerics/main.cpp
@@ -1,24 +1,40 @@
 #include<iostream>
 using namespace std;
 
+//#define binary
+
 void main()
 {
 	setlocale(LC_ALL, "");
-
-	int n[5], d;
+    int d;
 	cout << "Введите десятичное число: "; cin >> d;
-    
-	for (int i = 0; d > 0; i++)
-	
-	{
-		n[i] = d % 2;
-		d = d / 2;
-    
-		cout << n[i];
-	}
-      
-   cout << endl;
- 
 
+#ifdef binary
+	int i;
+	const int n = 32;
+	int arr[n] = {};
+	for (i = 0; d > 0; i++)
+
+	{
+		arr[i] = d % 2;
+		d /= 2;
+
+	}
+	cout << "Преобразованное число в двоичной системе: ";
+	for (i = i - 1; i >= 0; i--)cout << arr[i];
+
+	cout << endl;
+
+#endif // binary
+
+	const int n = 8;
+	char hex[n] = {};
+	int i;
+	for (i = 0; d; d /= 16)
+		hex[i++] = d % 16;
+
+	for(i = i - 1; i >= 0; i--)
+	cout << char(hex[i] < 10 ? hex[i] + 48 : hex[i] + 55);
+	cout << endl;
 }
 
