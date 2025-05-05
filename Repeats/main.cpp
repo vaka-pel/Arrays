1e9d8f3ab37b2072e6d42255bdadfb4b14509673
Pelivan <Pelivan@DESKTOP-29KL5UV>
2025-05-05 22:32:42 +0300

    Repeats DONE

--- Repeats/main.cpp
+++ Repeats/main.cpp
@@ -3,6 +3,46 @@ using namespace std;
 
 void main()
 {
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n];
+	for (int i = 0; i < n; i++)
+	
+	{
+		arr[i] = rand() % 10;
+	}
+	for (int i = 0; i < n; i++)
+	
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
 
+	for (int i = 0; i < n; i++)
+	
+	{
+		bool met_before = false;
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i] == arr[j])
+			{
+				met_before = true;
+				break;
+		    }
+		}
 
+		if (met_before)continue;
+		
+			int count = 0;
+			for (int j = i + 1; j < n; j++)
+
+			{
+				if (arr[i] == arr[j])
+				{
+					count++;
+				}
+			}
+			if (count > 0)cout << "Значение " << arr[i] << "Повторяется " << count << "раз\n";
+		
+	}
 }
